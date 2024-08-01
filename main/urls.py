f57e34937814c180be4403e62389914aebfaf0cb
alexnegrya <alexnegrya@mail.ru>
2024-08-01 22:19:32 +0300

    add User auth route

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from .views import *
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path('auth/', csrf_exempt(UserListView.as_view()))
 ]
