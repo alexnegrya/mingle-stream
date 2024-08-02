ecb4c3937919ff752a3d94c0ad39e007de0b5a86
alexnegrya <alexnegrya@mail.ru>
2024-08-02 23:28:32 +0300

    create auth view, check user before login

--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import ListView
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 from .models import User
 from .forms import *
+
+
+class MainListView(ListView):
+    http_method_names = ['get']
+    template_name = 'main.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
 
 class UserListView(ListView):
@@ -11,11 +20,19 @@ class UserListView(ListView):
 
     def post(self, request, *args, **kwargs):
         if UserAuthForm(request.POST).is_valid():
-            user, is_created = User.objects.get_or_create(
-                username=request.POST['username'],
-                password=request.POST['password']
-            )
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                if authenticate(request, username=request.POST['username'], password=request.POST['password']):
+                    status_code = 200
+                else:
+                    return HttpResponse(status=401)
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    username=request.POST['username'],
+                    password=request.POST['password']
+                )
+                status_code = 201
             login(request, user)
-            return HttpResponse(status=201 if is_created else 200)
+            return HttpResponse(status=status_code)
         else:
             return HttpResponseBadRequest()
