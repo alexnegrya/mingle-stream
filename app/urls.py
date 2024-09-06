12ef7cdbd7905c5186993540586b14c23e5bcd4d
alexnegrya <alexnegrya@mail.ru>
2024-09-06 19:00:21 +0300

    create main app view

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 
 urlpatterns = [
-
+    path('', AppView.as_view(), name='app_page')
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
+from django.views.generic import View
+from django.http import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseServerError,
+    JsonResponse
+)
 from django.shortcuts import render
 
-# Create your views here.
+
+class AppView(View):
+    http_method_names = ['get']
+    template_name = 'app.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            'avatar': request.user.img_url
+        })
