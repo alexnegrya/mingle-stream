7706116f96ec4d4ea965c728cd9d786bbbc7ddbb
alexnegrya <alexnegrya@mail.ru>
2024-09-06 23:36:09 +0300

    use decorator instead of simple func

--- main/admin.py
+++ main/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-
+@admin.register(User)
 class UserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + ((None, {"fields": ["img_url"]}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ["img_url"]}),)
-
-admin.site.register(User, UserAdmin)
