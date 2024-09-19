cacb089160da896a1c37ac6616524deda59f1e93
alexnegrya <alexnegrya@mail.ru>
2024-09-19 21:50:33 +0300

    add routes for chats and members operations

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 
 urlpatterns = [
-    path('', AppView.as_view(), name='app_page')
+    path('', AppView.as_view(), name='app_page'),
+
+    path('user-chats/', ChatsView.as_view({'get': 'get_user_chats'}), name='user_chats'),
+    path('create-chat/', ChatsView.as_view({'post': 'create_chat'}), name='create_chat'),
+    path('change-chat/', ChatsView.as_view({'patch': 'update_chat'}), name='change_chat'),
+    path('delete-chat/', ChatsView.as_view({'delete': 'delete_chat'}), name='delete_chat'),
+
+    path('chat-members/', ChatMembersView.as_view({'get': 'get_chat_members_usernames'}), name='chat_members'),
+    path('add-chat-member/', ChatMembersView.as_view({'post': 'add_chat_member'}), name='add_chat_member'),
+    path('delete-chat-member/', ChatMembersView.as_view({'delete': 'remove_chat_member'}), name='delete_chat_member')
 ]
