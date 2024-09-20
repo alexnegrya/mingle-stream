8a65e5ac1896947f80d36bc4b45b3b03120b4e26
alexnegrya <alexnegrya@mail.ru>
2024-09-20 18:52:56 +0300

    write Messages Django model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,8 @@ class Chats(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     color = models.CharField(max_length=6, null=False, blank=False,
         validators=[validate_hex_str], default=get_random_hex_color)
-    img_url = models.URLField(null=False, blank=False, default='https://i.ibb.co/Pc1nGqJ/placeholder.png')
+    img_url = models.URLField(null=False, blank=False,
+        default='https://i.ibb.co/Pc1nGqJ/placeholder.png')
     description = models.TextField(null=True, blank=True)
     bg_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(null=False, blank=False,
@@ -29,3 +30,18 @@ class ChatsMembers(models.Model):
 
     class Meta:
         verbose_name_plural = 'chats members'
+
+
+class Messages(models.Model):
+    chat_member = models.ForeignKey(ChatsMembers, models.CASCADE,
+        null=False, blank=False)
+    text = models.TextField(null=True, blank=True)
+    img_url = models.URLField(null=True, blank=True)
+    youtube_video_id = models.CharField(max_length=11, null=True, blank=True)
+    file_url = models.URLField(null=True, blank=True)
+    mixcloud_key = models.CharField(max_length=150, null=True, blank=True)
+    created_at = models.DateTimeField(null=False, blank=False,
+        auto_now_add=True, editable=False)
+    
+    class Meta:
+        verbose_name_plural = 'messages'
