5f0333d9da48ba95207ed562ea33c06acce4eda5
alexnegrya <alexnegrya@mail.ru>
2025-02-12 14:24:00 +0200

    set image modal args

--- app/static/js/image_modal.js
+++ app/static/js/image_modal.js
@@ -1,3 +1,9 @@
-function showFullscreenImage() {
-    document.getElementById('image_modal_fullscreen_img').style.display = 'block'
+function setImageModalArgs(model, imgUrl, args=null) {
+    $('#image_modal_model').attr('value', model)
+    $('#image_modal_img').attr('src', imgUrl)
+    $('#image_modal_args').attr('value', args)
 }
+
+// function showFullscreenImage() {
+//     document.getElementById('image_modal_fullscreen_img').style.display = 'block'
+// }
